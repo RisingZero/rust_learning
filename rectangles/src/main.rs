c277632d4f7edfcde38ce3f15d2e4696d6315590
RisingZero <ar.angelo259@gmail.com>
2022-07-11 14:01:23 +0200

    rectangles/different implementations and tests

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,3 +1,42 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    // First, two variables
+    let width1 = 10;
+    let height1 = 20;
+    println!("Width 2 variables");
+    println!("The area of the rectangle is {} square pixels", area1(width1,height1));
+
+    // With tuples
+    let rect2 = (10,20);
+    println!("Width tuples");
+    println!("The area of the rectangle is {} square pixels", area2(rect2));
+
+    // With structs
+    let rect3 = Rectangle {
+        width: 10,
+        height: 20,
+    };
+    println!("With structs");
+    println!("The area of the rectangle is {} square pixels", area3(&rect3));
+    println!("Rectangle is {:#?}", rect3); // rect3 is still valid because we passed reference, print debug information implemented with the derive Debug
+    let rect3 = dbg!(rect3);
+    // or
+    dbg!(&rect3); // dbg! doesn't take ownership
+}
+
+fn area1(width: u32, height: u32) -> u32 {
+    width * height
+}
+
+fn area2(dimensions: (u32,u32)) -> u32 {
+    dimensions.0 * dimensions.1
+}
+
+fn area3(rect: &Rectangle) -> u32 {
+    rect.width * rect.height
 }
